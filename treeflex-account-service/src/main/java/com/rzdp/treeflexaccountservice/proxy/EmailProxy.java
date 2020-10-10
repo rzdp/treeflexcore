56655bde10dec372d2ead99809085c381b910d68
xrriz <rriz.pena@gmail.com>
2020-10-10 19:28:55 +0800

    Fix account service bug

--- treeflex-account-service/src/main/java/com/rzdp/treeflexaccountservice/proxy/EmailProxy.java
+++ treeflex-account-service/src/main/java/com/rzdp/treeflexaccountservice/proxy/EmailProxy.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "treeflex-email-service", url = "http://localhost:8085")
+@FeignClient(name = "treeflex-email-service", url = "http://email-service:8085")
 public interface EmailProxy {
 
     @GetMapping("/v1/email")
