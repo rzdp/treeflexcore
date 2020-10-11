f96c589b049cbde49b06dc8b51c1083fd007c336
xrriz <rriz.pena@gmail.com>
2020-10-11 11:04:03 +0800

    Register Microservice with Service Discovery Server

--- treeflex-account-service/src/main/java/com/rzdp/treeflexaccountservice/TreeflexAccountServiceApplication.java
+++ treeflex-account-service/src/main/java/com/rzdp/treeflexaccountservice/TreeflexAccountServiceApplication.java
@@ -2,12 +2,14 @@ package com.rzdp.treeflexaccountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableResourceServer
+@EnableEurekaClient
 public class TreeflexAccountServiceApplication {
 
     public static void main(String[] args) {
--- treeflex-email-service/src/main/java/com/rzdp/treeflexemailservice/TreeflexEmailServiceApplication.java
+++ treeflex-email-service/src/main/java/com/rzdp/treeflexemailservice/TreeflexEmailServiceApplication.java
@@ -5,10 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
+@EnableEurekaClient
 public class TreeflexEmailServiceApplication {
 
     public static void main(String[] args) {
